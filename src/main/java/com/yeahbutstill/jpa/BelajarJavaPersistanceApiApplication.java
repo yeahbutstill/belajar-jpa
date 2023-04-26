e5ec30e1874c9bc9f2961e7f65dd2af8bc886e8c
yeahbutstill <mayadani28@gmail.com>
2023-04-26 07:35:56 +0700

    setup

---
+++ src/main/java/com/yeahbutstill/jpa/BelajarJavaPersistanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaPersistanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaPersistanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeahbutstill/jpa/BelajarJavaPersistanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaPersistanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
